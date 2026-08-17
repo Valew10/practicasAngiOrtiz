a6463a8179e3c7ff90d81d632b9cc34896dcd7a1
Valew10 <Angi.ortiz2027@frech.superate.org>
2026-08-17 10:53:43 -0600

    ....

---
+++ PracticasJs/Practica1JS/js/script.js
@@ -0,0 +1,17 @@
+document.write ("HOLA MUNDO");
+alert("MUNDO HOLA");
+console.log("HOLA CHAVALES");
+
+//Tipos de datos
+let nombre = "Ana";
+let edad ="18";
+let estudiante = true;
+let ciudad; 
+let mascota = null;
+
+//Usando typeof para identificar cada tipo
+console.log (typeof nombre);
+console.log (typeof edad);
+console.log (typeof estudiante);
+console.log (typeof ciudad);
+console.log (typeof mascota);
